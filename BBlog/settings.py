351988e47547413555bd05f652436d707b069703
minaduki123 <black4567june@yahoo.co.jp>
2020-03-05 02:54:36 +0900

    first commit

--- BBlog/settings.py
+++ BBlog/settings.py
@@ -166,13 +166,12 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-
-# メディアファイルの設定。
-DEFAULT_FILE_STORAGE = 'myBlog.backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = S3_URL
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+
+
 
