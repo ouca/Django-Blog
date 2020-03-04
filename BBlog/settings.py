fcfcd2214d3e3d9eb48a4884f02f563df8c02a16
minaduki123 <black4567june@yahoo.co.jp>
2020-03-05 02:22:46 +0900

    first commit

--- BBlog/settings.py
+++ BBlog/settings.py
@@ -132,9 +132,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+'''
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+'''
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
