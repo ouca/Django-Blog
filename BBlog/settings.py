d42a62f280790e45920dd356aa55177a0d7773f0
minaduki123 <black4567june@yahoo.co.jp>
2020-03-05 20:40:03 +0900

    two-commit

--- BBlog/settings.py
+++ BBlog/settings.py
@@ -155,11 +155,11 @@ except ImportError:
 
 if not DEBUG:
     SECRET_KEY = os.environ['*******']
-    # GOOGLE_ANALYTICS_TRACKING_ID = os.environ['GOOGLE_ANALYTICS_TRACKING_ID']
+
     AWS_ACCESS_KEY_ID = '*************'
     AWS_SECRET_ACCESS_KEY = '*******************'
     AWS_STORAGE_BUCKET_NAME = '*************'
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3-ap-northeast-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = '*************' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
     }
